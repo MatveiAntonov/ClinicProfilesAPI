dc0c7245a1148779231e1645dec525a690f96357
ProsTuha <tuhalf2002@mail.ru>
2023-01-24 16:34:42 +0300

    Added repositories di

--- Profiles.WebApi/Program.cs
+++ Profiles.WebApi/Program.cs
@@ -1,7 +1,13 @@
+using Profiles.Domain.Interfaces;
+using Profiles.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
+
 services.AddControllers();
+
+services.AddRepositories();
 
 var app = builder.Build();
 
