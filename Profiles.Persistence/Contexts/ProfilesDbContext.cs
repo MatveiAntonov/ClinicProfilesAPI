356f5143e05898391755a217e4303041d9df3539
ProsTuha <tuhalf2002@mail.ru>
2023-01-24 16:34:09 +0300

    Added db context

---
+++ Profiles.Persistence/Contexts/ProfilesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Profiles.Domain.Entities;
+using Profiles.Domain.Entities.ForeignEntities;
+using Profiles.Persistence.EntityTypeConfigurations;
+
+namespace Profiles.Persistence.Contexts
+{
+    public class ProfilesDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Receptionist> Receptionists { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Specialization> Specializations { get; set; }
+        public ProfilesDbContext(DbContextOptions<ProfilesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AccountConfiguration());
+            builder.ApplyConfiguration(new PatientProfileConfiguration());
+            builder.ApplyConfiguration(new ReceptionistProfileConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
