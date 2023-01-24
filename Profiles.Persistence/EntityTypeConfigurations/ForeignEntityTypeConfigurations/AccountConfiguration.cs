c55ce8d6a6d67e34339cd469e63c43e51da53b4a
ProsTuha <tuhalf2002@mail.ru>
2023-01-24 16:32:59 +0300

    Added foreign entities configuration

---
+++ Profiles.Persistence/EntityTypeConfigurations/ForeignEntityTypeConfigurations/AccountConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Profiles.Domain.Entities;
+using Profiles.Domain.Entities.ForeignEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Persistence.EntityTypeConfigurations.ForeignEntityTypeConfigurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(account => account.Id);
+            builder.HasIndex(account => account.Id)
+                .IsUnique();
+            builder.Property(account => account.PhoneNumber)
+                .HasMaxLength(13)
+                .IsRequired(false);
+        }
+    }
+}
