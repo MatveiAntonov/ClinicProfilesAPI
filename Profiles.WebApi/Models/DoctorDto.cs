7cba89851214b5c36ffc835e530c557b5a15310d
ProsTuha <tuhalf2002@mail.ru>
2023-01-24 16:34:25 +0300

    Added entities dtos

---
+++ Profiles.WebApi/Models/DoctorDto.cs
@@ -0,0 +1,18 @@
+﻿using Profiles.Domain.Entities.ForeignEntities;
+
+namespace Profiles.WebApi.Models
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string MiddleName { get; set; } = String.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public DateTime? CareerStartYear { get; set; }
+        public int Status { get; set; }
+        public Office? Office { get; set; }
+        public Specialization Specialization { get; set; } = new();
+        public Account Account { get; set; } = new();
+    }
+}
