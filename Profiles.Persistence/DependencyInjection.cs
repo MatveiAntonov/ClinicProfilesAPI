f0f535d0fb76271c8bd2e7b77c10ddf51e816278
ProsTuha <tuhalf2002@mail.ru>
2023-01-24 16:34:55 +0300

    Added di for repositories

---
+++ Profiles.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Profiles.Domain.Interfaces;
+using Profiles.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IReceptionistRepository, ReceptionistRepository>();
+            return services;
+        }
+    }
+}
