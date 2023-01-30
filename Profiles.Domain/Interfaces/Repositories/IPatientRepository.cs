c3aa2a295cbd56ec3a08a3625083f81ede50b904
ProsTuha <tuhalf2002@mail.ru>
2023-01-30 19:46:57 +0300

    Added profiles repositories

---
+++ Profiles.Domain/Interfaces/Repositories/IPatientRepository.cs
@@ -0,0 +1,18 @@
+﻿using Profiles.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Domain.Interfaces.Repositories
+{
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patient?>> GetAllPatients(CancellationToken cancellationToken);
+        Task<Patient?> GetPatient(int id, CancellationToken cancellationToken);
+        Task<Patient?> CreatePatient(Patient patient, CancellationToken cancellationToken);
+        Task<Patient?> UpdatePatient(Patient patient, CancellationToken cancellationToken);
+        Task<int> DeletePatient(int id, CancellationToken cancellationToken);
+    }
+}
---
+++ Profiles.Domain/Interfaces/Repositories/IReceptionistRepository.cs
@@ -0,0 +1,18 @@
+﻿using Profiles.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Domain.Interfaces.Repositories
+{
+    public interface IReceptionistRepository
+    {
+        Task<IEnumerable<Receptionist?>> GetAllReceptionists(CancellationToken cancellationToken);
+        Task<Receptionist?> GetReceptionist(int id, CancellationToken cancellationToken);
+        Task<Receptionist?> CreateReceptionist(Receptionist receptionist, CancellationToken cancellationToken);
+        Task<Receptionist?> UpdateReceptionist(Receptionist receptionist, CancellationToken cancellationToken);
+        Task<int> DeleteReceptionist(int id, CancellationToken cancellationToken);
+    }
+}
