89022454c8e9dfaab1b622a6693b1cbd42b0ab7b
ProsTuha <tuhalf2002@mail.ru>
2023-01-30 19:53:29 +0300

    Added program logic

--- Profiles.WebApi/Program.cs
+++ Profiles.WebApi/Program.cs
@@ -1,13 +1,25 @@
-using Profiles.Domain.Interfaces;
+using Profiles.WebApi.Mappings;
+using Profiles.Application.Services;
+using Profiles.WebApi.Extensions;
+using Profiles.Domain.Interfaces.Services;
 using Profiles.Persistence;
+using Profiles.Persistence.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
+services.AddSingleton<ProfilesDbContext>();
+
+services.AddRepositories();
+
+services.AddScoped<IDoctorService, DoctorService>();
+services.AddScoped<IPatientService, PatientService>();
+services.AddScoped<IReceptionistService, ReceptionistService>();
+
+services.AddAutoMapper(typeof(MappingProfile));
 
 services.AddControllers();
 
-services.AddRepositories();
 
 var app = builder.Build();
 
@@ -16,5 +28,7 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
+
+app.ConfigureExceptionHandler();
 
 app.Run();
