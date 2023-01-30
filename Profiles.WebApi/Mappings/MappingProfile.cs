e46951bf85b060e23f8ae51c2d9677875e54ed85
ProsTuha <tuhalf2002@mail.ru>
2023-01-30 19:53:14 +0300

    Added MappingProfile

---
+++ Profiles.WebApi/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Profiles.Domain.Entities;
+using Profiles.WebApi.Models.DTOs;
+
+namespace Profiles.WebApi.Mappings; 
+public class MappingProfile : Profile {
+    public MappingProfile()
+    {
+        CreateMap<Doctor, DoctorDto>().ReverseMap();
+        CreateMap<Patient, PatientDto>().ReverseMap();
+        CreateMap<Receptionist, ReceptionistDto>().ReverseMap();
+    }
+}
