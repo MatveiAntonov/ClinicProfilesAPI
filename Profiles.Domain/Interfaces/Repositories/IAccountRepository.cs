72fefa2971d04be5bbad3cbbd133769da5e0451b
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:17:11 +0300

    Added interface for account repository

---
+++ Profiles.Domain/Interfaces/Repositories/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Profiles.Domain.Entities;
+using Profiles.Domain.Entities.ForeignEntities;
+
+namespace Profiles.Domain.Interfaces.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<bool> CreateAccount(Account account);
+        Task<bool> UpdateAccountAsync(Account account);
+        Task<bool> DeleteAccountAsync(Account account);
+    }
+}
