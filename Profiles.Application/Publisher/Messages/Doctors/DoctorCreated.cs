539f475da7ae78d85ac0232f77bfc28f035f61a6
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:12:35 +0300

    Added messages for doctor publisher

---
+++ Profiles.Application/Publisher/Messages/Doctors/DoctorCreated.cs
@@ -0,0 +1,18 @@
+﻿namespace Events
+{
+    public class DoctorCreated
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; } = string.Empty;
+        public string SpecializationName { get; set; } = string.Empty;
+        public string PhotoUrl { get; set; } = string.Empty;
+        public string? OfficeName { get; set; } = string.Empty;
+        public string OfficeCity { get; set; } = string.Empty;
+        public string OfficeRegion { get; set; } = string.Empty;
+        public string OfficeStreet { get; set; } = string.Empty;
+        public string? OfficePostalCode { get; set; } = string.Empty;
+        public int OfficeHouseNumber { get; set; }
+    }
+}
