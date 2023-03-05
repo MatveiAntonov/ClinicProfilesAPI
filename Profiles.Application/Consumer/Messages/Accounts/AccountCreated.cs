bdc365f9c48030a0966bde7a3d18d0c88530fb5f
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:11:56 +0300

    Added messages for accounts entity

---
+++ Profiles.Application/Consumer/Messages/Accounts/AccountCreated.cs
@@ -0,0 +1,11 @@
+﻿using Profiles.Domain.Entities.ForeignEntities;
+
+namespace Events
+{
+    public class AccountCreated
+    {
+        public string Id { get; set; }
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string Url { get; set; } = String.Empty;
+    }
+}
