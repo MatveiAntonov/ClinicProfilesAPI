c9be4d30f980234af7076e27a4e4b29d9f1e67ec
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:11:39 +0300

    Added events for Accounts entity

---
+++ Profiles.Application/Consumer/Events/Accounts/AccountCreatedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Events;
+using MassTransit;
+using Profiles.Domain.Entities.ForeignEntities;
+using Profiles.Domain.Interfaces.Repositories;
+
+namespace Profiles.Application.Consumer.Events.Accounts
+{
+    public class AccountCreatedConsumer : IConsumer<AccountCreated>
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IMapper _mapper;
+
+        public AccountCreatedConsumer(IAccountRepository accountRepository, IMapper mapper)
+        {
+            _accountRepository = accountRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Consume(ConsumeContext<AccountCreated> context)
+        {
+            var account = _mapper.Map<Account>(context.Message);
+            if (account is not null)
+            {
+                await _accountRepository.CreateAccount(account);
+            }
+        }
+    }
+}
---
+++ Profiles.Application/Consumer/Events/Accounts/AccountUpdatedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Events;
+using MassTransit;
+using Profiles.Domain.Entities.ForeignEntities;
+using Profiles.Domain.Interfaces.Repositories;
+
+namespace Profiles.Application.Consumer.Events.Accounts
+{
+    public class AccountUpdatedConsumer : IConsumer<AccountUpdated>
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IMapper _mapper;
+        public AccountUpdatedConsumer(IAccountRepository accountRepository, IMapper mapper)
+        {
+            _accountRepository = accountRepository;
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<AccountUpdated> context)
+        {
+            var account = _mapper.Map<Account>(context.Message);
+            if (account is not null)
+            {
+                _accountRepository.UpdateAccountAsync(account);
+            }
+        }
+    }
+}
