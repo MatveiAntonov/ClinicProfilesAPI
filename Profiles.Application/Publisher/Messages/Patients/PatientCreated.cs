4d885ca5288236bd2b0bd81a0102337e1c19d9bf
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 21:13:09 +0300

    Added messages for patient publisher

---
+++ Profiles.Application/Publisher/Messages/Patients/PatientCreated.cs
@@ -0,0 +1,12 @@
+﻿namespace Events
+{
+    public class PatientCreated
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? MiddleName { get; set; } = String.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; } = string.Empty;
+    }
+}
